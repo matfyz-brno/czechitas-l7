dca26a7865c1d8fb869e6d7d77a97f3ead102b13
Eva Vašicová <miev@seznam.cz>
2023-03-02 19:17:39 +0100

    https://github.com/matfyz-brno/email-zadani.git

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 /*
 Místo pro váš program
 */
+
+
+/*const passwordUser = prompt('Zadejte heslo: ')
+let message = null
+
+if (passwordUser === 'Bagr') {
+	message = 'Přístup povolen'
+} else {
+	message = 'Přístup odepřen'
+}
+
+const answer = document.querySelector('#msg')
+answer.textContent= message*/
+/*
+const funkce_jedna = () => {
+    return 1
+}
+
+const vypis_funkci_jedna = () => {
+    console.log(funkce_jedna())
+    console.log(funkce_jedna())
+}
+*/
+/*
+const showOrderSummary = (summary) => {
+	const summaryElm = document.querySelector('#summary')
+	summaryElm.innerHTML = ` 
+    <div class="summary__count">Celkem položek: ${summary.count}</div>
+    <div class="summary__sum">Částka: ${summary.sum}</div>
+  ` /*ten innersummary donuti zapsat do html informace s div, oba radky*/
+
